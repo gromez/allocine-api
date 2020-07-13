b8f98b0dd8d44fc49e7230f223c0ea7f3d8c138b
David <github.site@torick.net>
2020-07-13 17:40:33 -0400

    feat(csharp): Update to netstd 2.0

--- CS/AlloCineAPI/AlloCineApi.cs
+++ CS/AlloCineAPI/AlloCineApi.cs
@@ -11,7 +11,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
-using System.Device.Location;
 
 namespace AlloCine
 {
--- CS/AlloCineAPI/ExtensionMethods.cs
+++ CS/AlloCineAPI/ExtensionMethods.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
